1811d2c4ceabc4619ee75e2a5ac7b01e80e89082
Kieran T <kieran123416@gmail.com>
2023-10-26 18:56:18 +0100

    Framework Pushed

---
+++ Source/HalloweenGameJam/HalloweenGameJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HalloweenGameJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HALLOWEENGAMEJAM_API AHalloweenGameJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
